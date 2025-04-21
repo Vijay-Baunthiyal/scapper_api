144a4b55b7dba4352faa5c35f67b7723c1c49799
Vijay Baunthiyal <43056494+Vijay-Baunthiyal@users.noreply.github.com>
2025-04-21 09:39:49 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,61 @@
+
+############ varified code ################
+
+# from fastapi import FastAPI, Depends, HTTPException, Request
+# from fastapi.security import APIKeyHeader
+
+# app = FastAPI()
+
+# # Set your desired API key here
+# API_KEY = "QcpkfjphlC19vJoUfwDadLbmZDrmFdjCsEjfdmmc1YQlmw6SKl"
+# api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
+
+# # Auth function
+# async def verify_api_key(api_key: str = Depends(api_key_header)):
+#     if api_key != API_KEY:
+#         raise HTTPException(status_code=403, detail="Invalid API Key")
+#     return api_key
+
+# def run_main_script_logic():
+#     return {"status": "Script logic executed", "data": {'a':1, 'b':2, 'c':3}}
+
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Welcome to the public endpoint"}
+
+# # Secure route
+# @app.get("/secure")
+# async def secure_route(api_key: str = Depends(verify_api_key)):
+#     result = run_main_script_logic()
+#     return result
+
+
 from fastapi import FastAPI, Depends, HTTPException, Request
 from fastapi.security import APIKeyHeader
 
 app = FastAPI()
 
 # Set your desired API key here
-API_KEY = "QcpkfjphlC19vJoUfwDadLbmZDrmFdjCsEjfdmmc1YQlmw6SKl"
-api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
+Main = "ng-app=CRMPortal"
+Main_header = APIKeyHeader(name="Header", auto_error=False)
 
 # Auth function
-async def verify_api_key(api_key: str = Depends(api_key_header)):
-    if api_key != API_KEY:
-        raise HTTPException(status_code=403, detail="Invalid API Key")
-    return api_key
+async def scanning_webpage(header: str = Depends(Main_header)):
+    if header != Main:
+        raise HTTPException(status_code=403, detail="HTML Error")
+    return header
 
-def run_main_script_logic():
-    return {"status": "Script logic executed", "data": None}
+def webpage_parsing():
+    return {"status": "parsing completed", "webpage_elements": {'addcomment': 'div.btn-group:nth-child(7) > button:nth-child(2)', 'b':2, 'c':3}}
+
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the public endpoint"}
+    return {"message": ""}
 
 # Secure route
 @app.get("/secure")
-async def secure_route(api_key: str = Depends(verify_api_key)):
-    result = run_main_script_logic()
+async def secure_route(header: str = Depends(scanning_webpage)):
+    result = webpage_parsing()
     return result
