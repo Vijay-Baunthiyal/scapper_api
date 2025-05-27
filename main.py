852ffa11c0e53d159502463ded26a221a441fe0d
Vijay Baunthiyal <43056494+Vijay-Baunthiyal@users.noreply.github.com>
2025-05-27 02:22:51 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -62,9 +62,13 @@ def webpage_parsing():
 
     return {"status": "parsing completed", "webpage_elements": {'div1': 'div.btn-group:nth-child(7) > button:nth-child(2)', 'div2':'div.btn-group:nth-child(7) > ul:nth-child(3) > li:nth-child(1) > span:nth-child(1) > a:nth-child(1) > ng-transclude:nth-child(1) > a:nth-child(1)'
     , 'div3':'#text', 'div4':'body > div.modal.fade.app-modal-window.in > div > div > div.modal-footer > button.btn.btn-default', 'div5':'div.btn', 'div6':'div.btn', 'div7':'div.btn', 'div8':'div.btn'}
-    , "webform": {'dynamic' :'Keys.TAB', 'div1': '#crmportal-container > div > div > div.main.crmportal-content-main.col-xs-offset-3.col-md-offset-2 > div > workspace > div.row > tile:nth-child(16) > div > div.panel-heading.ws-tile-heading-container > div.pull-right.ws-tile-options.btn-group > button.btn.btn-primary.btn-xs.glyphicon.glyphicon-plus.dropdown-toggle'
-                                        ,'div2': '#crmportal-container > div > div > div.main.crmportal-content-main.col-xs-offset-3.col-md-offset-2 > div > workspace > div.row > tile:nth-child(16) > div > div.panel-heading.ws-tile-heading-container > div.pull-right.ws-tile-options.btn-group.open > ul > li:nth-child(2) > span > a'
-                                        ,'div3': '#amount', 'div4': '#targetAccountId > div > div > input'}}
+    , "webform": {'dynamic' :'Keys.TAB', 'pay_adjust': '#crmportal-container > div > div > div.main.crmportal-content-main.col-xs-offset-3.col-md-offset-2 > div > workspace > div.row > tile:nth-child(16) > div > div.panel-heading.ws-tile-heading-container > div.pull-right.ws-tile-options.btn-group > button.btn.btn-primary.btn-xs.glyphicon.glyphicon-plus.dropdown-toggle'
+                                        , 'payment_inputs': '#crmportal-container > div > div > div.main.crmportal-content-main.col-xs-offset-3.col-md-offset-2 > div > workspace > div.row > tile:nth-child(16) > div > div.panel-heading.ws-tile-heading-container > div.pull-right.ws-tile-options.btn-group.open > ul > li:nth-child(2) > span > a'
+                                        , 'amount': '#amount', 'target_account': '#targetAccountId > div > div > input', 'trasfer_pay': "//*[@id='Adjustment.Category.GroupedReasons_57']"
+                                        , 'continue': '//*[@id="targetAccountId"]/div/div/input', 'notes': '#notes'
+                                        , 'process': '//*[@id="Actions"]/div/div/form/div/div[2]/button[1]'
+                                        , 'payment_process': '#Actions > div > div:nth-child(3) > form > div > div.btn-group-vertical > button.btn.btn.success'
+                                        , 'submit': '#Actions > div > div:nth-child(5) > form > div > div.btm-group-vertical > button' }}
 
 
 @app.get("/")
